1cd098811855a0a2dfb1aaf99ef52cd33fb95249
Ale <AROrtega@hotmail.com>
2023-10-11 15:46:13 -0300

    practica y compresion de metodo .this en objetos

---
+++ objetos-this.js
@@ -0,0 +1,68 @@
+// =============== [DEFINICION] ===============
+
+//con 'this' podemos acceder a las keys propias del objeto en el que estemos trabajando.
+//obteniendo valores propios del objeto en donde llamamos a 'this'
+
+// =============== [PRACTICA 1] ===============
+let persona = {
+    nombre: 'Ale',
+    apellido: 'Ortega',
+    nacionalidad: 'Argentina',
+    mascotas: ['Fioni', 'Princess', 'Pampi'],
+    nombrecompleto: this.nombre + ' ' + this.apellido,
+    direccion: {
+        ciudad: 'Viedma',
+        pais: 'Argentina',
+        calle: 'Maipu',
+        altura: 1425
+    }
+};
+
+//console.log(persona);
+//console.log(persona.apellido);
+//console.log(persona.direccion);
+//console.log(persona.direccion.altura);
+
+// =============== [PRACTICA 2] ===============
+
+const cuenta = {
+    numero: '392112',
+    saldo: 1500,
+    depositar(cantidad) {
+        this.saldo = this.saldo + cantidad
+    },
+    extraer(cantidad) {
+        this.saldo = this.saldo - cantidad
+    }
+};
+
+//console.log(cuenta);
+//cuenta.depositar(500);
+//cuenta.depositar(250);
+//console.log(cuenta);
+//cuenta.extraer(900);
+//console.log(cuenta);
+
+// =============== [PRACTICA 3] ===============
+
+const perfil = {
+    numeroFavorito: 13,
+    apodo: 'Dark',
+    colorFavorito: 'Morado',
+    estacionFavorita: 'Invierno',
+    cambioDeNumeroFavorito(nuevoNumero) {
+        this.numeroFavorito = nuevoNumero
+    },
+    cambioDeApodo(nuevoApodo) {
+        this.apodo = nuevoApodo
+    },
+    cambioDeColorFavorito(nuevoColor) {
+        this.colorFavorito = nuevoColor
+    }
+};
+
+console.log(perfil);
+perfil.cambioDeNumeroFavorito(19);
+perfil.cambioDeApodo('Grey'),
+perfil.cambioDeColorFavorito('Rojo')
+console.log(perfil);
