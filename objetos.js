0a281ce5866a350c9af7a8cafd93ef87e971a8fc
Ale <AROrtega@hotmail.com>
2023-10-10 21:47:22 -0300

    objeto .js para completar y practicar metodos

---
+++ objetos.js
@@ -0,0 +1,9 @@
+let carnet = {
+    nombre: 'Ale',
+    apellido: 'Ortega',
+    nacionalidad: 'Argentina',
+    mascotas: ['Fioni', 'Princesa', 'Pampi']
+};
+
+console.log(carnet);
+console.log(carnet.apellido);
